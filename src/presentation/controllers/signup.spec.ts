b4fd977bbbbe43a856e6f5702019f9df7afd82a3
Evandro Calado <evandrocalado07@gmail.com>
2024-10-05 22:18:43 -0300

    test: ensure SignupController return 400 if no name is provider

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it } from 'vitest';
+import { SignupController } from './signup';
+
+describe('Signup Controller', () => {
+  it('should return 400 if no name is provided', () => {
+    const sut = new SignupController();
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
