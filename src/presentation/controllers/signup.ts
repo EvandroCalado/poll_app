9902422378f5d12984f21868418a62d42c32fb58
Evandro Calado <evandrocalado07@gmail.com>
2024-10-05 22:12:56 -0300

    feat: ensure signup controller return 400 if no name provider

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export class SignupController {
+  handle(httpRequest: any): any {
+    return {
+      statusCode: 400,
+    };
+  }
+}
